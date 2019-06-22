8b6025a8024ca3e9815f70716324d23b457dd544
Syed Hussain Ather <shussainather@gmail.com>
2019-06-21 22:31:34 -0400

    def fit

--- boost.py
+++ boost.py
@@ -7,4 +7,16 @@ class Boosting:
         self.models = None
         self.accuracy = []
         self.predictions = None
-    
+
+    def fit(self):
+        # Set the descriptive features and the target feature
+        X = self.dataset.drop(['target'],axis=1)
+        Y = self.dataset['target'].where(self.dataset['target']==1,-1)
+        # Initialize the weights of each sample with wi = 1/N and create a dataframe in which the evaluation is computed
+        Evaluation = pd.DataFrame(Y.copy())
+        Evaluation['weights'] = 1/len(self.dataset) # Set the initial weights w = 1/N
+        
+        # Run the boosting algorithm by creating T "weighted models"
+        alphas = [] 
+        models = []
+         
