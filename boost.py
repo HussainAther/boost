73bf605fb810d0ceb5fcd10a96cdb0de675cd015
Syed Hussain Ather <shussainather@gmail.com>
2019-06-21 22:30:23 -0400

    init

---
+++ boost.py
@@ -0,0 +1,10 @@
+class Boosting:
+    def __init__(self,dataset,T,test_dataset):
+        self.dataset = dataset
+        self.T = T
+        self.test_dataset = test_dataset
+        self.alphas = None
+        self.models = None
+        self.accuracy = []
+        self.predictions = None
+    
