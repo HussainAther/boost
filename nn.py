33a22c274bd1c6428120fa778312b884b917ab8e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-08 22:08:25 -0400

    train

--- nn.py
+++ nn.py
@@ -38,4 +38,34 @@ class NeuralNetwork:
         X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
         self.weights_hidden_out = X.rvs((self.no_of_out_nodes, 
                                         self.no_of_hidden_nodes))
-    
+   
+    def train(self, input_vector, target_vector):
+        """
+        input_vector and target_vector can be tuple, list or ndarray.
+        """
+        bias_node = 1 if self.bias else 0
+        if self.bias:
+            # adding bias node to the end of the inpuy_vector
+            input_vector = np.concatenate( (input_vector, [self.bias]) )
+        input_vector = np.array(input_vector, ndmin=2).T
+        target_vector = np.array(target_vector, ndmin=2).T
+        output_vector1 = np.dot(self.weights_in_hidden, input_vector)
+        output_vector_hidden = activation_function(output_vector1)
+        if self.bias:
+            output_vector_hidden = np.concatenate( (output_vector_hidden, [[self.bias]]) )
+        output_vector2 = np.dot(self.weights_hidden_out, output_vector_hidden)
+        output_vector_network = activation_function(output_vector2)
+        output_errors = target_vector - output_vector_network
+        # update the weights:
+        tmp = output_errors * output_vector_network * (1.0 - output_vector_network)     
+        tmp = self.learning_rate  * np.dot(tmp, output_vector_hidden.T)
+        self.weights_hidden_out += tmp
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.weights_hidden_out.T, output_errors)
+        # update the weights:
+        tmp = hidden_errors * output_vector_hidden * (1.0 - output_vector_hidden)
+        if self.bias:
+            x = np.dot(tmp, input_vector.T)[:-1,:]     # ???? last element cut off, ???
+        else:
+            x = np.dot(tmp, input_vector.T)
+        self.weights_in_hidden += self.learning_rate * x 
