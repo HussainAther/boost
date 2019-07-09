08289be663e7edd943cf4a9b1635b32bffc5ee52
Syed Hussain Ather <shussainather@gmail.com>
2019-07-08 22:05:06 -0400

    class

--- nn.py
+++ nn.py
@@ -12,3 +12,16 @@ def truncated_normal(mean=0, sd=1, low=0, upp=10):
     Use the built-in scipy function to truncate.
     """
     return truncnorm((low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+class NeuralNetwork:
+           
+    def __init__(self, 
+                 no_of_in_nodes, 
+                 no_of_out_nodes, 
+                 no_of_hidden_nodes,
+                 learning_rate):
+        self.no_of_in_nodes = no_of_in_nodes
+        self.no_of_out_nodes = no_of_out_nodes
+        self.no_of_hidden_nodes = no_of_hidden_nodes
+        self.learning_rate = learning_rate 
+        self.create_weight_matrices()
